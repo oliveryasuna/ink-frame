d878c4bf80c3ef316c02f676eba8f39869488a4d
Oliver Yasuna <oliveryasuna.acct@gmail.com>
2026-08-23 21:22:46 -0400

    Fixed lint-staged.

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -2,5 +2,5 @@ import type {Configuration} from 'lint-staged';
 
 export default ({
   '*': ((): string => 'secretlint "**/*"'),
-  'src/**/*.{js,mjs,cjs,jsx,ts,mts,cts,tsx,json,md,html,css,scss}': ((): string => 'tt verify')
+  './**/*.{js,mjs,cjs,jsx,ts,mts,cts,tsx,json,md,html,css,scss}': ((): string => 'tt verify')
 } satisfies Configuration);
